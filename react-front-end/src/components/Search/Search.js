50e9a014de1a48298e58f1ebdbaedb21f13fda56
Allen <headhuntar@gmail.com>
2020-05-10 12:31:21 -0700

    found better animation already existing in react material

--- react-front-end/src/components/Search/Search.js
+++ react-front-end/src/components/Search/Search.js
@@ -1,44 +1,23 @@
 import React, { useState } from 'react';
-import { Transition } from 'react-transition-group';
+import Collapse from '@material-ui/core/Collapse';
 import MyMapComponent from '../GoogleMaps/index';
-
-const duration = 300;
-
-const defaultStyle = {
-  transition: `opacity ${duration}ms ease-in-out`,
-  opacity: 0,
-}
-
-const transitionStyles = {
-  entering: { opacity: 1 },
-  entered: { opacity: 1 },
-  exiting: { opacity: 0 },
-  exited: { opacity: 0 },
-};
 
 const Search = () => {
   const [inProp, setInProp] = useState(false);
   return (
     <div>
       <h1>Search Route!</h1>
-      <Transition in={inProp} timeout={500}>
-        {state => (
-          <div style={{
-            ...defaultStyle,
-            ...transitionStyles[state]
-          }}>
-            <MyMapComponent
-              googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
-              loadingElement={<div style={{ height: `100%` }} />}
-              containerElement={<div style={{ height: `600px` }} />}
-              mapElement={<div style={{ height: `100%` }} />}
-            />
-          </div>
-        )}
-      </Transition>
+      <Collapse in={inProp}>
+        <MyMapComponent
+          googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `600px` }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+        />
+      </Collapse>
       <button onClick={() => setInProp(inProp ? false : true)}>
         animate show . hide
-        </button>
+      </button>
     </div>
   )
 };
