f2c052193ce120ccffd05c4531241fec5526119d
Allen <headhuntar@gmail.com>
2020-05-10 11:57:09 -0700

    implement example of animation fade

--- react-front-end/src/components/Search/Search.js
+++ react-front-end/src/components/Search/Search.js
@@ -1,16 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Transition } from 'react-transition-group';
 import MyMapComponent from '../GoogleMaps/index';
 
+const duration = 300;
+
+const defaultStyle = {
+  transition: `opacity ${duration}ms ease-in-out`,
+  opacity: 0,
+}
+
+const transitionStyles = {
+  entering: { opacity: 1 },
+  entered: { opacity: 1 },
+  exiting: { opacity: 0 },
+  exited: { opacity: 0 },
+};
+
 const Search = () => {
+  const [inProp, setInProp] = useState(false);
   return (
     <div>
       <h1>Search Route!</h1>
-      <MyMapComponent
-        googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
-        loadingElement={<div style={{ height: `100%` }} />}
-        containerElement={<div style={{ height: `600px` }} />}
-        mapElement={<div style={{ height: `100%` }} />}
-      />
+      <Transition in={inProp} timeout={500}>
+        {state => (
+          <div style={{
+            ...defaultStyle,
+            ...transitionStyles[state]
+          }}>
+            <MyMapComponent
+              googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+              loadingElement={<div style={{ height: `100%` }} />}
+              containerElement={<div style={{ height: `600px` }} />}
+              mapElement={<div style={{ height: `100%` }} />}
+            />
+          </div>
+        )}
+      </Transition>
+      <button onClick={() => setInProp(inProp ? false : true)}>
+        animate show . hide
+        </button>
     </div>
   )
 };
