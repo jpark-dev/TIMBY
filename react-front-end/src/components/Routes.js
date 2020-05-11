74a97c7929089987b6cdfcdbb10a0280adf30fe1
Junsung Park <zeipar@gmail.com>
2020-05-11 07:21:05 +0000

    Refactored routing component from App.js. Also removed <BotNav> component from router for better practice.

---
+++ react-front-end/src/components/Routes.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import Search from './Search/Search';
+import Listings from './Listings/Listings';
+import Profile from './Profile/Profile';
+import Bookings from './Bookings/Bookings';
+import BotNav from './BotNav';
+
+export default function Routes() {
+
+  return (
+    <Router>
+      <Switch>
+        <Route path="/search">
+          <Search />
+        </Route>
+        <Route path="/bookings">
+          <Bookings />
+        </Route>
+        <Route path="/listings">
+          <Listings />
+        </Route>
+        <Route path="/profile">
+          <Profile />
+        </Route>
+      </Switch>
+    </Router>
+  );
+};
