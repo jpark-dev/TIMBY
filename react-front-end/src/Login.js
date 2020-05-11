8162984bbe76c834e67044bd68b98b2902205ce9
Dexter Chan <dexterchann@gmail.com>
2020-05-11 15:20:05 -0700

    add helper functions for fetching notifications

--- react-front-end/src/Login.js
+++ react-front-end/src/Login.js
@@ -1,32 +1,31 @@
 import React, { useState } from 'react';
 
 const Login = (props) => {
-
-  const [username, setUsername] = useState(null);
-  const [password, setPassword] = useState(null);
+  const [notifications, setNotifications] = useState([]);
+  
   const userID = localStorage.getItem('userID');
 
-  const login = (username, password) => {
-    fetch(`${process.env.REACT_APP_API_URL}/login`, {
-      method: 'POST',
-      body: JSON.stringify({ username, password }),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-    .then(res => res.json())
-    .then(data => {
-      if (data.status < 400) {
-        console.log(data);
-        localStorage.setItem('userID', data.userID);
-        return true;
-      }
-      return false;
-    });
-  };
+  // const login = (username, password) => {
+  //   fetch(`${process.env.REACT_APP_API_URL}/login`, {
+  //     method: 'POST',
+  //     body: JSON.stringify({ username, password }),
+  //     headers: {
+  //       'Content-Type': 'application/json'
+  //     }
+  //   })
+  //   .then(res => res.json())
+  //   .then(data => {
+  //     if (data.status < 400) {
+  //       console.log(data);
+  //       localStorage.setItem('userID', data.userID);
+  //       return true;
+  //     }
+  //     return false;
+  //   });
+  // };
 
-  const notifyUser = (id, text) => {
-    fetch(`${process.env.REACT_APP_API_URL}/notifications/send/${id}`, {
+  const notifyUser = (userID, text) => {
+    fetch(`${process.env.REACT_APP_API_URL}/notifications/send/${userID}`, {
       method: 'POST',
       body: JSON.stringify({ text }),
       headers: {
@@ -38,16 +37,29 @@ const Login = (props) => {
     .catch(error => console.log(error));
   }
 
+  const fetchNotificationsForUser = (userID) => {
+    fetch(`${process.env.REACT_APP_API_URL}/notifications/${userID}`, {
+      method: 'GET'
+    })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      setNotifications(data);
+    })
+    .catch(error => console.log(error));
+  };
+
   return (
     <>
-      <h1>Login Component</h1>
+      <h1>TIMBY</h1>
       {userID && <h2>Hello user with ID {userID}</h2>}
-      <form onSubmit={e => { e.preventDefault(); login(username, password); }}>
-        <input onChange={e => setUsername(e.target.value)} placeholder='username' />
-        <input onChange={e => setPassword(e.target.value)} type='password' placeholder='password' />
-        <button>Login</button>
-      </form>
       <button onClick={() => notifyUser(localStorage.getItem('userID'), "Notification from button!")}>Notify!</button>
+      <button onClick={() => fetchNotificationsForUser(localStorage.getItem('userID'))}>Fetch Notifications</button>
+      <div>
+        {notifications.map((notification) => (
+          <p key={notification.id}>{notification.body}</p>
+        ))}
+      </div>
     </>
   );
 }
