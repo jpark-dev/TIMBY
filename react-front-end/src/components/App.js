36dfba9b1f1567a6265b5dc27fa9cd773ac5b0fa
Allen <headhuntar@gmail.com>
2020-05-12 11:18:06 -0700

    added appbar, some basic styling

--- react-front-end/src/components/App.js
+++ react-front-end/src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+
 import { Link, Route, Switch } from 'react-router-dom';
 import Search from './Search/Search';
 import Listings from './Listings/Listings';
@@ -8,23 +9,45 @@ import Bookings from './Bookings/Bookings';
 
 import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
 import Drawer from '@material-ui/core/Drawer';
-import Button from '@material-ui/core/Button';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import SearchIcon from '@material-ui/icons/Search';
+import BookmarkIcon from '@material-ui/icons/Bookmark';
+import StoreIcon from '@material-ui/icons/Store';
+import MenuIcon from '@material-ui/icons/Menu';
+import PersonIcon from '@material-ui/icons/Person';
+import NotificationsIcon from '@material-ui/icons/Notifications';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   list: {
     width: 250,
   },
   fullList: {
     width: 'auto',
   },
-});
+  link: {
+    textDecoration: 'none',
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  transparent: {
+    backgroundColor: 'transparent',
+    color: '#000',
+    boxShadow: 'none',
+  }
+}));
 
 
 export default function App() {
@@ -47,34 +70,57 @@ export default function App() {
       onKeyDown={toggleDrawer(false)}
     >
       <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-            <ListItemText primary={text} />
+        <Link to="/profile" className={classes.link} >
+          <ListItem button key='profile'>
+            <ListItemIcon><AccountCircleIcon /></ListItemIcon>
+            <ListItemText primary='Profile' />
           </ListItem>
-        ))}
+        </Link>
+        <Link to="/search" className={classes.link} >
+          <ListItem button key='search'>
+            <ListItemIcon><SearchIcon /></ListItemIcon>
+            <ListItemText primary='search' />
+          </ListItem>
+        </Link>
+        <Link to="/bookings" className={classes.link} >
+          <ListItem button key='bookings'>
+            <ListItemIcon><BookmarkIcon /></ListItemIcon>
+            <ListItemText primary='bookings' />
+          </ListItem>
+        </Link>
+        <Link to="/listings" className={classes.link} >
+          <ListItem button key='listings'>
+            <ListItemIcon><StoreIcon /></ListItemIcon>
+            <ListItemText primary='listings' />
+          </ListItem>
+        </Link>
       </List>
     </div>
   );
 
   return (
-    <div>
-      <React.Fragment >
-        <Button onClick={toggleDrawer(true)}>open</Button>
-        <Drawer open={state} onClose={toggleDrawer(false)}>
-          {list()}
-        </Drawer>
-      </React.Fragment>
-      <main>
-        <Switch>
-          <Route path="/search" component={Search} />
-          <Route path="/bookings" component={Bookings} />
-          <Route path="/listings" component={Listings} />
-          <Route path="/profile" component={Profile} />
-          <Route path="/" exact component={Search} />
-          {/* <Route component={ErrorComponent} /> */}
-        </Switch>
-      </main>
-    </div>
+    <main>
+      <AppBar position="static" className={classes.transparent}>
+        <Toolbar>
+          <IconButton className={classes.menuButton} onClick={toggleDrawer(true)}><MenuIcon /></IconButton>
+          <Drawer open={state} onClose={toggleDrawer(false)}>
+            {list()}
+          </Drawer>
+          <Typography variant="h6" className={classes.title}>
+            TIMBY
+          </Typography>
+          <IconButton className={classes.menuButton}><PersonIcon /></IconButton>
+          <IconButton className={classes.menuButton}><NotificationsIcon /></IconButton>
+        </Toolbar>
+      </AppBar>
+      <Switch>
+        <Route path="/search" component={Search} />
+        <Route path="/bookings" component={Bookings} />
+        <Route path="/listings" component={Listings} />
+        <Route path="/profile" component={Profile} />
+        <Route path="/" exact component={Search} />
+        {/* <Route component={ErrorComponent} /> */}
+      </Switch>
+    </main>
   );
 };
