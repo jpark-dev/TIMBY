9aec55898da5435efa85188f49fbee0de44fad1f
Dexter Chan <dexterchann@gmail.com>
2020-05-12 02:08:25 +0000

    clean up semicolon and add comment

--- express-back-end/routes/notifications.js
+++ express-back-end/routes/notifications.js
@@ -5,11 +5,9 @@ const webpush = require('web-push');
 module.exports = (db) => {
   // FETCH ALL NOTIFICATIONS FOR A USER
   router.get("/:id", (req, res) => {
-    // console.log("Hit get notifications route!");
     db.getNotificationsForUser(req.params.id)
       .then(data => data.rows)
       .then(rows => {
-        // console.log("ALL NOTIFICATIONS", rows);
         res.send(rows);
       })
       .catch(err => {
@@ -22,11 +20,8 @@ module.exports = (db) => {
   // SUBSCRIBE USER TO PUSH NOTIFICATIONS
   router.post('/subscribe', (req, res) => {
     const subscription = req.body.subscription;
-    // console.log("hit the subscribe route!");
-    // console.log(req.body);
   
     if (req.body.userID) {
-      // console.log("attempting to add push subscription to db");
       db.addPushSubscription(req.body.userID, subscription)
         .then((data) => {
           console.log(`New push subscription added to user ${data.rows[0].id}`);
@@ -36,8 +31,6 @@ module.exports = (db) => {
         })
     }
   
-    // console.log(subscription);
-  
     const payload = JSON.stringify({
       title: 'TIMBY',
       body: 'Welcome to TIMBY!',
@@ -46,22 +39,17 @@ module.exports = (db) => {
     webpush.sendNotification(subscription, payload)
       .then(result => {;
         console.log("Notification sent from server!");
-        // console.log(result);
       })
       .catch(e => console.log(e.stack))
-  
-    res.status(200).json({'success': true})
+      
+    // Indicate successful response to client
+    res.status(200).json({'success': true});
   });
 
   // SEND NOTIFICATION TO USER
   router.post("/send/:id", (req, res) => {
-    console.log("Hit send notification route!");
-    // console.log(req.body);
-    // console.log(req.params.id);
     db.addNotification(req.params.id, req.body.text)
       .then(() => {
-  
-        // console.log("Notification added to database!");
   
         const payload = JSON.stringify({
           title: 'TIMBY',
@@ -72,9 +60,8 @@ module.exports = (db) => {
           .then(data => data.rows[0].push_subscription)
           .then(push_subscription => {
             webpush.sendNotification(push_subscription, payload)
-            .then(result => {;
+            .then(result => {
               console.log(`Notification sent from server to user ${req.params.id}`);
-              // console.log(result);
               res.send(`Notification sent from server to user ${req.params.id}`);
             })
             .catch(e => console.log(e.stack))
