31437649e29487c879075e91d0d0d1c0718f405b
Allen <headhuntar@gmail.com>
2020-05-12 10:17:28 -0700

    added drawers n routes to app

--- react-front-end/src/components/App.js
+++ react-front-end/src/components/App.js
@@ -1,22 +1,80 @@
 import React from 'react';
 import './App.css';
-import Routes from './Routes';
+import { Link, Route, Switch } from 'react-router-dom';
+import Search from './Search/Search';
+import Listings from './Listings/Listings';
+import Profile from './Profile/Profile';
+import Bookings from './Bookings/Bookings';
+
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import Button from '@material-ui/core/Button';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+
+const useStyles = makeStyles({
+  list: {
+    width: 250,
+  },
+  fullList: {
+    width: 'auto',
+  },
+});
+
 
 export default function App() {
-  // const [msg, setMsg] = useState("Click the button to load data!");
+  const classes = useStyles();
+  const [state, setState] = React.useState(false);
 
-  // const fetchData = () => {
-  //   axios.get('/api/data') // You can simply make your requests to "/api/whatever you want"
-  //   .then((response) => {
-  //     // handle success
-  //     console.log(response.data) // The entire response from the Rails API
+  const toggleDrawer = (open) => (event) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
 
-  //     console.log(response.data.message) // Just the message
-  //     setMsg(response.data.message)
-  //   })
-  // };
+    setState(open);
+  };
+
+  const list = () => (
+    <div
+      className={clsx(classes.list)}
+      role="presentation"
+      onClick={toggleDrawer(false)}
+      onKeyDown={toggleDrawer(false)}
+    >
+      <List>
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
 
   return (
-    <Routes />
+    <div>
+      <React.Fragment >
+        <Button onClick={toggleDrawer(true)}>open</Button>
+        <Drawer open={state} onClose={toggleDrawer(false)}>
+          {list()}
+        </Drawer>
+      </React.Fragment>
+      <main>
+        <Switch>
+          <Route path="/search" component={Search} />
+          <Route path="/bookings" component={Bookings} />
+          <Route path="/listings" component={Listings} />
+          <Route path="/profile" component={Profile} />
+          <Route path="/" exact component={Search} />
+          {/* <Route component={ErrorComponent} /> */}
+        </Switch>
+      </main>
+    </div>
   );
 };
