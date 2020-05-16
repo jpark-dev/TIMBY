46b761c376416b029a05b137fc8920ef4ac396d0
Dexter Chan <dexterchann@gmail.com>
2020-05-16 15:17:44 -0700

    display loading spinner while bookings are being fetched

--- react-front-end/src/components/Bookings/Bookings.js
+++ react-front-end/src/components/Bookings/Bookings.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import LoadingSpinner from './LoadingSpinner';
 import fetchBookingsForUser from '../../helpers/fetchBookingsForUser';
 import Booking from './Booking';
 
@@ -11,12 +12,14 @@ export default function Bookings () {
     fetchBookingsForUser(localStorage.getItem('userID'))
       .then(bookings => {
         setBookings(bookings);
-      });
+      })
+      .catch(error => console.log(error));
   }, []);
 
   return(
     <div className="bookings-container">
-      {bookings.map(booking => (
+      {!bookings[0] && <LoadingSpinner />}
+      {bookings && bookings.map(booking => (
         <Booking key={booking.id} {...booking} />
       ))}
     </div>
