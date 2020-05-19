8e95843484d42a62822801c06a82eaf51c90612b
Dexter Chan <dexterchann@gmail.com>
2020-05-19 04:45:57 +0000

    update seed data

--- express-back-end/db/seeds/03_bookings_seed.sql
+++ express-back-end/db/seeds/03_bookings_seed.sql
@@ -1,17 +1,16 @@
 -- Users table seeds here (Example)
 INSERT INTO bookings (tour_id, user_id, comment, rating, created_at, status)
-  VALUES (1, 2, NULL, NULL, '2020-05-15 10:00:00', 'Confirmed');
+  VALUES (2, 2, NULL, NULL, '2020-05-15 10:00:00', 'Confirmed');
 
 INSERT INTO bookings (tour_id, user_id, comment, rating, created_at, status)
-  VALUES (1, 4, NULL, NULL, '2020-05-15 10:00:00', 'Confirmed');
+  VALUES (3, 4, NULL, NULL, '2020-05-15 10:00:00', 'Confirmed');
 
 INSERT INTO bookings (tour_id, user_id, comment, rating, created_at, status)
-  VALUES (2, 5, NULL, NULL, '2020-05-15 10:00:00', 'Confirmed');
+  VALUES (3, 1, NULL, NULL, '2020-05-15 10:00:00', 'Confirmed');
 
 INSERT INTO bookings (tour_id, user_id, comment, rating, created_at, status)
-  VALUES (2, 6, NULL, NULL, '2020-05-15 10:00:00', 'Confirmed');
+  VALUES (4, 6, NULL, NULL, '2020-05-15 10:00:00', 'Completed');
 
 INSERT INTO bookings (tour_id, user_id, comment, rating, created_at, status)
-  VALUES (4, 7, NULL, NULL, '2020-05-15 10:00:00', 'Completed'),
-  (3, 7, NULL, NULL, '2020-05-15 10:00:00', 'Confirmed');
+  VALUES (2, 7, NULL, NULL, '2020-05-15 10:00:00', 'Confirmed');
   
