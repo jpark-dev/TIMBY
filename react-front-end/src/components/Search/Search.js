dcbe6689291d92790d288fa1f90f34c913ca660f
Allen <headhuntar@gmail.com>
2020-05-19 09:46:16 -0700

    updated styling to match jasons

--- react-front-end/src/components/Search/Search.js
+++ react-front-end/src/components/Search/Search.js
@@ -54,7 +54,7 @@ const Search = () => {
       <MyMapComponent
         googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_MAP_API_KEY}&v=3.exp&libraries=geometry,drawing,places`}
         loadingElement={<div style={{ height: `100%` }} />}
-        containerElement={<div style={{ height: window.innerHeight - 56, width: '100%' }} />}
+        containerElement={<div style={{ height: window.innerHeight, width: '100%' }} />}
         mapElement={<div style={{ height: `100%` }} />}
         // mapElement={<div style={{ height: `100%`, zIndex: -1 }} />}
         markers={markers}
